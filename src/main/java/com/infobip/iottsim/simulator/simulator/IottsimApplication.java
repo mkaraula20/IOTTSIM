8ff5059f556bd9f5e7d9167f2db8e9630cdadc65
Matija Karaula <Matija.Karaula@infobip.com>
2022-12-15 16:19:28 +0100

    Initial commit

---
+++ src/main/java/com/infobip/iottsim/simulator/simulator/IottsimApplication.java
@@ -0,0 +1,13 @@
+package com.infobip.iottsim.simulator.simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IottsimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IottsimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/infobip/iottsim/simulator/simulator/IottsimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infobip.iottsim.simulator.simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IottsimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
